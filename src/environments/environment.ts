3ede8381a8294e439e3ab485262f529e4ffa3b9a
I-Phalafala <ituphala@gmail.com>
2024-09-14 03:57:17 +0200

    Remove Twilio configuration files for WhatsApp integration

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: false,
-    twilioAccountSid: 'AC4118d7de9985ab38101ecb9dee22b448',
-    twilioAuthToken: 'a13596e9a33c64819b90af0617649431',
-    twilioWhatsAppNumber: '+27600129933'
-};
